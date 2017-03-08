d691a5d17fccdd8dffc36321eae9a43c13bb422b
Alix <fullertonalix0@gmail.com>
2017-03-07 19:13:28 -0700

    Adding Pi on  boot

---
+++ tools/pi_on_boot.py
@@ -0,0 +1,15 @@
+import subprocess
+
+from politeauthority.raspi import Raspi
+
+connected = Raspi().get_local()
+ssid = Raspi().get_current_ssid('wlan0')
+wan_ip = Raspi().get_wan()
+print connected
+
+message = "Connected to %s at IP:%s, Gateway:%s, WAN: %s" % (ssid, connected[0], connected[1], wan_ip)
+print message
+cmd = """curl -X POST -H 'Content-type: application/json' --data '{"text":"%s"}' https://hooks.slack.com/services/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""" % (message)
+subprocess.call(cmd, shell=True)
+
+
