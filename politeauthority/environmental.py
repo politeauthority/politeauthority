67eb8277574dcd9b54c05b82fd0bd956dd2979ef
Alix <fullertonalix0@gmail.com>
2017-03-13 15:20:27 -0600

    Moving twitter and mysql configuration to env

--- politeauthority/environmental.py
+++ politeauthority/environmental.py
@@ -24,4 +24,44 @@ def get_machine_id():
         'PA_MACHINE_ID',
         'UNKNOWN')
 
+###############################################
+#              MySQL CRAP                   #
+###############################################
+
+
+def mysql_conf():
+    return {
+        'host': os.environ.get('PA_MYSQL_HOST', None),
+        'user': os.environ.get('PA_MYSQL_USER', None),
+        'pass': os.environ.get('PA_MYSQL_PASS', None),
+    }
+
+###############################################
+#              TWITTER CRAP                   #
+###############################################
+
+
+def twitter_consumer_key():
+    return os.environ.get(
+        'PA_TWITTER_CONSUMER_KEY',
+        None)
+
+
+def twitter_consumer_secret():
+    return os.environ.get(
+        'PA_TWITTER_CONSUMER_SECRET',
+        None)
+
+
+def twitter_access_key():
+    return os.environ.get(
+        'PA_TWITTER_ACCESS_KEY',
+        None)
+
+
+def twitter_access_secret():
+    return os.environ.get(
+        'PA_TWITTER_ACCESS_SECRET',
+        None)
+
 # End File: politeauthority/politeauthority/environmental.py
