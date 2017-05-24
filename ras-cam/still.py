7fa9f07c9fae935e7633489afdac3baad2a86c89
Alix <fullertonalix0@gmail.com>
2017-05-24 16:00:28 -0600

    Updating ras-cam to remove source files if it shipped its file

--- ras-cam/still.py
+++ ras-cam/still.py
@@ -2,20 +2,33 @@
 
 import subprocess
 from datetime import datetime
-print datetime.now()
-file_name = '/home/pi/photos/temp_%s.jpg' % str(datetime.now())[:19].replace(' ', 'T').replace(':', '_')
+
+photo_name = 'temp_'
+file_name = '/home/pi/photos/%s%s.jpg' % (
+    photo_name,
+    str(datetime.now())[:19].replace(' ', 'T').replace(':', '_')
+    )
 cmd = ['raspistill',
        '-rot 270',
        '-o',
-       file_name,
-       '']
-print ' '.join(cmd)
+       file_name]
+
+print 'Taking picture'
 subprocess.check_output(
         ' '.join(cmd),
         shell=True)
+picture_uploaded = None
+print 'Trying to send the picture'
 try:
     cmd = "scp %s alix@chatsec.org:~/photos" % file_name
     subprocess.check_output(cmd, shell=True)
+    picture_uploaded = True
+    print 'Uploaded the picture'
 except Exception, e:
+    print 'Failed to send the picture'
     print e
+    picture_uploaded = False
 
+if picture_uploaded:
+    subprocess.check_output('rm %s' % file_name)
+    print 'Picture Removed'
