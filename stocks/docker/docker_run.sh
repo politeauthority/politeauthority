e973894c2d9729c696f998f02cfdd317b8f053e2
Alix Fullerton <alix@politeauthority.com>
2017-07-14 14:08:22 -0600

    Removing docker run

--- stocks/docker/docker_run.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    -v /Users/alix/repos/politeauthority:/opt/politeauthority \
-    -e PA_MYSQL_HOST="138.197.194.190" \
-    -e PA_MYSQL_USER="devel" \
-    -e PA_MYSQL_PASS="78VWc_bKTAap" \
-    -e PA_STOCKS_CONFIG='/opt/politeauthority/stocks/flask/configs/config_dev.py' \
-    politestocks
