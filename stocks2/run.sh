b35828e111b46fb97754e66cbb9db4a690cb048c
Alix Fullerton <alix@politeauthority.com>
2017-09-07 20:06:54 -0600

    Updating for live deployment

--- stocks2/run.sh
+++ stocks2/run.sh
@@ -1,15 +1,14 @@
-echo 'TRYING!'
-if [[ -z "${PA_BUILD}" ]]; then
-  RUN_ENV="DEV"
+if [ "${PA_BUILD}" == "LIVE" ]; then
+  RUN_ENV="LIVE"
 else
   RUN_ENV="${PA_BUILD}"
 fi
-echo 'NOWWW'
+
 if [ RUN_ENV="DEV" ]; then
 	echo 'Running Standalone'
 	python run.py
 else
 	echo 'Running with Gunicorn'
-	gunicorn -w 4 -b 127.0.0.1:5000 app:app
+	gunicorn -w 4 -b -D 127.0.0.1:5000 app:app
 fi
-echo 'DEAD'
+echo 'Stopped'
